cd0fa20d25cdfb0e01f6aaa0a2768cb9b902c536
Jeremy Karlsson <jeremy@barlingshult.se>
2025-09-17 11:03:08 +0200

    Fix rendering with nested categories in index page

--- _config.ts
+++ _config.ts
@@ -9,7 +9,54 @@ site.use(transform_images());
 site.add("/sgog");
 
 site.filter("getProvinces", (value) => {
-    return value[0].province;
+  return value[0].province;
+});
+
+interface SGOGPage extends Lume.Page {
+  province: string;
+  district: string;
+}
+
+site.filter("onlyProvince", (pages: SGOGPage[], province: string) => {
+  return pages.filter((page) => page.province === province);
+});
+
+site.filter("onlyDistrict", (pages: SGOGPage[], district: string) => {
+  return pages.filter((page) => page.district === district);
+});
+
+site.filter("getProvinces", (pages: SGOGPage[]) => {
+  const provinces: Record<string, any[]> = {};
+
+  for (const page of pages) {
+    const family = page.province;
+    provinces[family] = [...(provinces[family] || []), page];
+  }
+
+  return Object.entries(provinces)
+    .map(([province, pages]) => ({
+      name: province,
+      slug: pages[0].provinceSlug,
+      count: pages.length,
+    }))
+    .sort((a, b) => a.name.localeCompare(b.name));
+});
+
+site.filter("getDistricts", (pages: SGOGPage[]) => {
+  const districts: Record<string, any[]> = {};
+
+  for (const page of pages) {
+    const district = page.district;
+    districts[district] = [...(districts[district] || []), page];
+  }
+
+  return Object.entries(districts)
+    .map(([district, pages]) => ({
+      name: district,
+      slug: district.toLocaleLowerCase().split(' ').join('-'),
+      count: pages.length,
+    }))
+    .sort((a, b) => a.name.localeCompare(b.name));
 });
 
 export default site;
